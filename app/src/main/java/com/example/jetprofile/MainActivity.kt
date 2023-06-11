bd2819059d501105d5055e3f796ca527060ae4d9
OniRemon <saito4431@gmail.com>
2023-06-11 20:19:58 +0900

    UI一通り作成

--- app/src/main/java/com/example/jetprofile/MainActivity.kt
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.jetprofile
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,15 +58,97 @@ class MainActivity : ComponentActivity() {
                         Text(
                             text = "職業: Androidエンジニア",
                             color = Color.Gray,
-                            fontSize = 12.sp,
+                            fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold
                         )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Column(
+                            horizontalAlignment = Alignment.Start,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "エンジニア株式会社",
+                                fontSize = 26.sp,
+                                fontWeight = FontWeight.ExtraBold
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+                            Text(
+                                text = "エンジニア部",
+                                color = Color.Gray,
+                                fontSize = 16.sp
+                            )
+                            Spacer(modifier = Modifier.height(20.dp))
+                            
+                            Label(icon = Icons.Default.Email, text = "Email")
 
-                        
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(text = "oniremon@gmail.com", fontSize = 16.sp)
+                            Spacer(modifier = Modifier.height(5.dp))
+
+                            Divider(
+                                modifier = Modifier.clip(RoundedCornerShape(1000.dp)),
+                                thickness = 2.dp
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color(0xFFF85F6A)
+                            )
+                        ) {
+                            Text(
+                                text = "詳細を表示",
+                                color = Color.White
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clip(RoundedCornerShape(10.dp))
+                                .background(Color.LightGray.copy(alpha = 0.3f))
+                                .padding(horizontal = 10.dp, vertical = 20.dp)
+                        ) {
+                            Label(
+                                icon = Icons.Default.Favorite,
+                                text = "スノーボード、個人開発",
+                                color = Color.Gray
+                            )
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Label(
+                                icon = Icons.Default.LocationOn,
+                                text = "居住地：埼玉県さいたま市",
+                                color = Color.Gray
+                            )
+                        }
                     }
-
                 }
             }
         }
+    }
+}
+
+@Composable
+fun Label(
+    icon: ImageVector,
+    text: String,
+    color: Color = MaterialTheme.colors.onBackground
+){
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Icon(
+            imageVector = icon,
+            contentDescription = "email"
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = text,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = color
+        )
     }
 }
